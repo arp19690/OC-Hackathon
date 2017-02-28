d88c3947fa1070e78e2fd1271c81cd0573c319c7
Arpit Maheshwari <arp19690@gmail.com>
2017-02-28 18:07:09 +0530

    adding different views

--- app/views.py
+++ app/views.py
@@ -20,6 +20,29 @@ def get_ga_real_time_data(request):
                                                    datetime.now().strftime(
                                                        "%Y-%m-%d"))
 
+    orders_sold_per_minute = appDAL.get_orders_per_minutes(
+        str(datetime.now().strftime("%Y-%m-%d")) + " 00:00:00",
+        str(datetime.now().strftime("%Y-%m-%d %H:%M:%s")),
+        float(datetime.now().hour * datetime.now().minute))
+
+    data_context = {
+        "website": {
+            "total_users": total_website_users,
+            "all_sources": all_website_sources,
+            "top_page_views": top_page_views,
+        },
+        "orders_sold_per_minute": orders_sold_per_minute,
+    }
+
+    return render(request, "app/realtime-data.html", context=data_context)
+
+def get_ga_time_based_data(request):
+    top_page_views = googleAnalytics.get_pageviews(GA_WEBSITE_VIEW_ID,
+                                                   datetime.now().strftime(
+                                                       "%Y-%m-%d"),
+                                                   datetime.now().strftime(
+                                                       "%Y-%m-%d"))
+
     from_datetime = "2017-01-01 00:00:00"
     end_datetime = "2017-01-02 00:00:00"
     top_retail_customers = appDAL.get_top_retail_customers(from_datetime,
@@ -41,16 +64,12 @@ def get_ga_real_time_data(request):
         float(datetime.now().hour * datetime.now().minute))
 
     data_context = {
-        "website": {
-            "total_users": total_website_users,
-            "all_sources": all_website_sources,
-            "top_page_views": top_page_views,
-        },
         "top_retail_customers": top_retail_customers,
         "top_products_delivered": top_products_delivered,
         "top_customers_by_city": top_customers_by_city,
         "top_sellers": top_sellers,
         "orders_sold_per_minute": orders_sold_per_minute,
+        "top_page_views": top_page_views,
     }
 
-    return render(request, "app/realtime-data.html", context=data_context)
+    return render(request, "app/data-info.html", context=data_context)
