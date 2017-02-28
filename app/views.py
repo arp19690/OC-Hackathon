98a62eb3becccc320ea2d5653fa7d22f4f780e8f
Arpit Maheshwari <arp19690@gmail.com>
2017-02-28 20:07:31 +0530

    displaying traffic sources

--- app/views.py
+++ app/views.py
@@ -10,26 +10,37 @@ GA_APP_VIEW_ID = "ga:132813188"
 
 # Create your views here.
 def get_ga_real_time_data(request):
-    website_data = googleAnalytics.get_realtime_active_users(GA_WEBSITE_VIEW_ID)
+    website_data = googleAnalytics.get_realtime_active_users(
+        GA_WEBSITE_VIEW_ID)
     ga_app_data = googleAnalytics.get_realtime_active_users(GA_APP_VIEW_ID)
 
     total_website_users = website_data["totalsForAllResults"]["rt:activeUsers"]
-    all_website_sources = website_data["rows"]
+    all_website_sources = list()
+    if len(website_data["rows"]) > 0:
+        for tmpdata in website_data["rows"]:
+            tmpdict = {"device": tmpdata[1],
+                       "data": {"source": tmpdata[0], "count": tmpdata[2]}}
+            all_website_sources.append(tmpdict)
 
     total_app_users = ga_app_data["totalsForAllResults"]["rt:activeUsers"]
-    all_app_sources = ga_app_data["rows"]
+    all_app_sources = list()
+    if len(ga_app_data["rows"]) > 0:
+        for tmpdata in ga_app_data["rows"]:
+            tmpdict = {"device": tmpdata[1],
+                       "data": {"source": tmpdata[0], "count": tmpdata[2]}}
+            all_app_sources.append(tmpdict)
 
     top_website_page_views = googleAnalytics.get_pageviews(GA_WEBSITE_VIEW_ID,
-                                                   datetime.now().strftime(
-                                                       "%Y-%m-%d"),
-                                                   datetime.now().strftime(
-                                                       "%Y-%m-%d"))
+                                                           datetime.now().strftime(
+                                                               "%Y-%m-%d"),
+                                                           datetime.now().strftime(
+                                                               "%Y-%m-%d"))
 
     top_app_page_views = googleAnalytics.get_pageviews(GA_APP_VIEW_ID,
-                                                   datetime.now().strftime(
-                                                       "%Y-%m-%d"),
-                                                   datetime.now().strftime(
-                                                       "%Y-%m-%d"))
+                                                       datetime.now().strftime(
+                                                           "%Y-%m-%d"),
+                                                       datetime.now().strftime(
+                                                           "%Y-%m-%d"))
 
     orders_sold_per_minute = appDAL.get_orders_per_minutes(
         str(datetime.now().strftime("%Y-%m-%d")) + " 00:00:00",
@@ -51,6 +62,7 @@ def get_ga_real_time_data(request):
     }
 
     return render(request, "app/realtime-data.html", context=data_context)
+
 
 def get_ga_time_based_data(request):
     top_page_views = googleAnalytics.get_pageviews(GA_WEBSITE_VIEW_ID,
