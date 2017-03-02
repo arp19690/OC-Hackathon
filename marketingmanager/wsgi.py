e5ac62c262d298a92212bb849208c08f1e3104a2
Arpit Maheshwari <arp19690@gmail.com>
2017-03-02 10:49:33 +0530

    adding dotenv package

--- marketingmanager/wsgi.py
+++ marketingmanager/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+import dotenv
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "marketingmanager.settings")
-
+dotenv.read_dotenv(
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 application = get_wsgi_application()
